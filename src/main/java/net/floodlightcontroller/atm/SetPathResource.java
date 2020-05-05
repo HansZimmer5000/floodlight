c5642035c87566eb1ebb3a5b2a15fbe3236b219f
hanszimmer <michael.weitnau@live.de>
2020-05-05 18:20:00 +0200

    add switchService

--- src/main/java/net/floodlightcontroller/atm/SetPathResource.java
+++ src/main/java/net/floodlightcontroller/atm/SetPathResource.java
@@ -1,5 +1,7 @@
 package net.floodlightcontroller.atm;
 
+import net.floodlightcontroller.core.internal.IOFSwitchService;
+
 import org.restlet.data.Status;
 import org.restlet.resource.Put;
 import org.restlet.resource.ServerResource;
@@ -7,29 +9,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SetPathResource extends ServerResource {
-	protected static Logger log = LoggerFactory.getLogger(SetPathResource.class);
-	
+	protected static Logger log = LoggerFactory
+			.getLogger(SetPathResource.class);
+
 	@Put
-	public String SetPath(String jsonBody){
+	public String SetPath(String jsonBody) {
 		log.debug("SetPathReceived:" + jsonBody);
 		Status status = Status.CLIENT_ERROR_BAD_REQUEST;
 		String message = "Error unkown";
-		
-		IACIDUpdaterService updateService =
-                (IACIDUpdaterService)getContext().getAttributes().
-                    get(IACIDUpdaterService.class.getCanonicalName());
+
+		IACIDUpdaterService updateService = (IACIDUpdaterService) getContext()
+				.getAttributes().get(
+						IACIDUpdaterService.class.getCanonicalName());
+
+		IOFSwitchService switchService = (IOFSwitchService) getContext().getAttributes().get(IOFSwitchService.class.getCanonicalName());
 		
 		message = updateService.getName() + " " + updateService.getClass();
-		
-		// For Parameters: String param = (String) getRequestAttributes().get("switch");
-		
-		//TODO Extract Info from JSON
-		//rowValues = StaticEntries.jsonToStorageEntry(jsonBody);
-		
-		//TODO Set Path according to ASP
-		
+
+		// For Parameters: String param = (String)
+		// getRequestAttributes().get("switch");
+
+		// TODO Extract Info from JSON
+		// rowValues = StaticEntries.jsonToStorageEntry(jsonBody);
+
+		// TODO Set Path according to ASP
+
 		setStatus(status, message);
-		return message; // Returning null will still send a response but it seems broken / failure
+		return message; // Returning null will still send a response but it
+						// seems broken / failure
 	}
-	
+
 }
