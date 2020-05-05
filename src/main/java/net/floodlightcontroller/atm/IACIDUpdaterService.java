42e2b9be23d8f86c357a90df9c67e9f96aaecaa1
hanszimmer <michael.weitnau@live.de>
2020-05-05 20:26:23 +0200

    add functions

--- src/main/java/net/floodlightcontroller/atm/IACIDUpdaterService.java
+++ src/main/java/net/floodlightcontroller/atm/IACIDUpdaterService.java
@@ -1,9 +1,23 @@
 package net.floodlightcontroller.atm;
 
+import java.util.List;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+
 import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.internal.IOFSwitchService;
 import net.floodlightcontroller.core.module.IFloodlightService;
 
 public interface IACIDUpdaterService extends IFloodlightService, IOFMessageListener {
-	//TODO Set functions for update the network.
-
+	
+	public Byte[] createNewUpdateIDAndPrepareMessages();
+	
+	public List<MessagePair> getMessages(Byte[] updateID);
+	
+	public void voteLock(List<IOFSwitch> switches);
+	
+	public void rollback(List<IOFSwitch> switches);
+	
+	public void commit(List<IOFSwitch> switches);
 }
