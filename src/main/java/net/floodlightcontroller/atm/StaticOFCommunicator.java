48b01628d53315c96e970c8978938e56e9280122
hanszimmer <michael.weitnau@live.de>
2020-05-05 09:01:40 +0200

    add static communicator

---
+++ src/main/java/net/floodlightcontroller/atm/StaticOFCommunicator.java
@@ -0,0 +1,11 @@
+package net.floodlightcontroller.atm;
+
+import net.floodlightcontroller.core.internal.IOFSwitchService;
+
+
+public class StaticOFCommunicator {
+
+	public static boolean UpdateSwitch(IOFSwitchService swichService){
+		return false;
+	}
+}
