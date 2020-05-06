6daa8c6db9ec4c0cd2f1dba20077b4e258a73e6f
hanszimmer <michael.weitnau@live.de>
2020-05-06 17:36:27 +0200

    add first tests

---
+++ src/test/java/net/floodlightcontroller/atm/SetPathResourceTests.java
@@ -0,0 +1,106 @@
+package net.floodlightcontroller.atm;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.projectfloodlight.openflow.protocol.OFFlowAdd;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.types.EthType;
+
+import net.floodlightcontroller.test.FloodlightTestCase;
+
+public class SetPathResourceTests extends FloodlightTestCase {
+
+	private SetPathResource setPathResource;
+	private static String exampleDPID = "AA:BB:CC:DD:EE:FF:00:11";
+	private static String exampleFlowName = "flow-mode-1";
+	private static String exampleCookieBool = "0";
+	private static String examplePrio = "32768";
+	private static String exampleIngressPort = "1";
+	private static String exampleActions = "output=2";
+	private static String exampleJson = "{\n" + "\"switch\":       \""
+			+ exampleDPID + "\",\n" + "\"name\":         \"" + exampleFlowName
+			+ "\",\n" + "\"cookie\":       \"" + exampleCookieBool + "\",\n"
+			+ "\"priority\":     \"" + examplePrio + "\",\n"
+			+ "\"ingress-port\": \"" + exampleIngressPort + "\",\n"
+			+ "\"actions\":      \"" + exampleActions + "\"\n" + "}";
+
+	@Override
+	public void setUp() throws Exception {
+		this.setPathResource = new SetPathResource();
+	}
+
+	@Test
+	public void testgetUnfinishedSwitches() throws Exception {
+
+	}
+
+	@Test
+	public void testgetUnconfirmedSwitches() throws Exception {
+
+	}
+
+	@Test
+	public void testSetPath() throws Exception {
+
+	}
+
+	@Test
+	public void testextractSwitches() throws Exception {
+
+	}
+
+	@Test
+	public void testgextractHosts() throws Exception {
+
+	}
+
+	@Test
+	public void testcreateFlowMods() throws Exception {
+
+	}
+
+	@Test
+	public void testcreateFlowMod() throws Exception {
+		int outPort = 2;
+		long xid = 16;
+
+		OFFlowAdd testMod1 = this.setPathResource.createFlowMod(exampleJson, xid);
+
+		Assert.assertEquals("OF_14", testMod1.getVersion().toString());
+		Assert.assertEquals(outPort, testMod1.getOutPort().getPortNumber());
+		Assert.assertEquals(16, testMod1.getXid());
+		Assert.assertEquals("ADD", testMod1.getCommand().toString());
+
+		Assert.assertEquals(1, testMod1.getInstructions().size());
+		Assert.assertTrue(testMod1.getInstructions().get(0).toString()
+				.indexOf("port=" + String.valueOf(outPort)) > 0);
+
+		Assert.assertEquals(1, testMod1.getMatch().get(MatchField.IN_PORT)
+				.getPortNumber());
+		Assert.assertEquals(EthType.IPv4,
+				testMod1.getMatch().get(MatchField.ETH_TYPE));
+	}
+
+	@Test
+	public void testconvertJsonToMap() {
+
+		try {
+			Map<String, String> testMap = this.setPathResource
+					.convertJsonToMap(exampleJson);
+
+			Assert.assertEquals(exampleDPID, testMap.get("switch"));
+			Assert.assertEquals(exampleFlowName, testMap.get("name"));
+			Assert.assertEquals(exampleCookieBool, testMap.get("cookie"));
+			Assert.assertEquals(examplePrio, testMap.get("priority"));
+			Assert.assertEquals(exampleIngressPort, testMap.get("ingress-port"));
+			Assert.assertEquals(exampleActions, testMap.get("actions"));
+		} catch (IOException e) {
+			Assert.fail(e.toString());
+		}
+
+	}
+}
