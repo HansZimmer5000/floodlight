5a5fa0068380a1903f12f526f0fa56e4c0a5930e
hanszimmer <michael.weitnau@live.de>
2020-05-07 12:56:36 +0200

    add equals and tostring

--- src/main/java/net/floodlightcontroller/atm/FlowModDTO.java
+++ src/main/java/net/floodlightcontroller/atm/FlowModDTO.java
@@ -1,10 +1,35 @@
 package net.floodlightcontroller.atm;
 
+
 public class FlowModDTO {
 
 	public String dpid, name;
 	public int inPort, outPort;
+
+	public FlowModDTO() {
+	}
 	
-	public FlowModDTO(){}
+	public String toString(){
+		String format = "FlowMod: dpid({1}) name({2}) inPort({3}) outPort({4})";
+		Object[] args = {this.dpid, this.name, this.inPort, this.outPort};
+		String output = String.format(format, args);
+		return output;
+	}
+
+	public boolean equals(Object obj) {
+		boolean result = false;
+
+		if (obj instanceof FlowModDTO) {
+			FlowModDTO objFM = (FlowModDTO) obj;
+			if (this.dpid.equals(objFM.dpid)
+					&& this.name.equals(objFM.name)
+					&& this.inPort == objFM.inPort
+					&& this.outPort == objFM.outPort) {
+				result = true;
+			}
+		}
+
+		return result;
+	}
 
 }
