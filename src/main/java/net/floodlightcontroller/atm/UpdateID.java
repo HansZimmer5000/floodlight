1e95670cd743d5a697bbff3ad50e8c0af1f0775d
hanszimmer <michael.weitnau@live.de>
2020-05-09 18:13:38 +0200

    add hash and equals

--- src/main/java/net/floodlightcontroller/atm/UpdateID.java
+++ src/main/java/net/floodlightcontroller/atm/UpdateID.java
@@ -1,62 +1,96 @@
 package net.floodlightcontroller.atm;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Random;
 
 public class UpdateID {
 
 	public byte atmID;
 	public byte[] restBytes;
-	
+
 	public UpdateID(int atmID) {
-		//byte[] bytes = ByteBuffer.allocate(1).putShort(atmID).array();
+		// byte[] bytes = ByteBuffer.allocate(1).putShort(atmID).array();
 		this.atmID = new Integer(atmID).byteValue();
 		this.restBytes = this.createRestBytes();
 	}
-	
+
 	public UpdateID(long atmID) {
-		//byte[] bytes = ByteBuffer.allocate(1).putShort(atmID).array();
+		// byte[] bytes = ByteBuffer.allocate(1).putShort(atmID).array();
 		this.atmID = new Long(atmID).byteValue();
 		this.restBytes = this.createRestBytes();
 	}
-	
-	public UpdateID(byte[] arr){
+
+	public UpdateID(byte[] arr) {
 		this.atmID = arr[0];
 		this.restBytes = new byte[3];
 		this.restBytes[0] = arr[1];
 		this.restBytes[1] = arr[2];
 		this.restBytes[2] = arr[3];
 	}
-	
-	public static byte createNewATMID(){
+
+	public static byte createNewATMID() {
 		byte[] result = new byte[1];
 		new Random().nextBytes(result);
 		return result[0];
 	}
-	
-	public byte[] createRestBytes(){
+
+	public byte[] createRestBytes() {
 		byte[] result = new byte[3];
-		
 		new Random().nextBytes(result);
-		
 		return result;
 	}
-	
-	public byte[] toArr(){
+
+	public byte[] toArr() {
 		byte[] result = new byte[4];
-		
+
 		result[0] = atmID;
 		result[1] = this.restBytes[0];
 		result[2] = this.restBytes[1];
 		result[3] = this.restBytes[2];
-		
+
 		return result;
 	}
-	
-	public String toString(){
-		String format = "UpdateID: atmID({1}) restBytes({2}))";
-		Object[] args = {this.atmID, this.restBytes};
-		String output = String.format(format, args);
+
+	public static UpdateID ofValue(long rawUpdateID) {
+		byte[] rawArr = new byte[4];
+
+		for (int i = 3; i >= 0; i--) {
+			rawArr[i] = (byte) (rawUpdateID & 0xFF);
+			rawUpdateID >>= 8;
+		}
+		return new UpdateID(rawArr);
+	}
+
+	public String toString() {
+		String atmStr = String.format("%02X", this.atmID);
+	    StringBuilder sb = new StringBuilder();
+	    for (byte b : this.restBytes) {
+	        sb.append(String.format("%02X ", b));
+	    }
+		String output = "UpdateID: atmID(" + atmStr + ") restBytes("
+				+ sb.toString() + "))";
 		return output;
+	}
+	
+	public int hashCode(){
+		int atmVal = new Byte(this.atmID).hashCode();
+		int restVal = Arrays.hashCode(this.restBytes);
+		
+		return atmVal + restVal;
+	}
+
+	public boolean equals(Object obj){
+		boolean result = false;
+		
+		if (obj instanceof UpdateID) {
+			UpdateID objID = (UpdateID) obj;
+			if (this.hashCode() == objID.hashCode()){
+				result = true;
+			}
+		}
+		
+		return result;
 	}
 	
 	public long toLong() {
@@ -66,10 +100,10 @@ public class UpdateID {
 		fullArr[1] = this.restBytes[0];
 		fullArr[2] = this.restBytes[1];
 		fullArr[3] = this.restBytes[2];
-		
+
 		for (int i = 0; i < fullArr.length; i++) {
 			result = result << 8;
-			byte tmpVal = fullArr[i];			
+			byte tmpVal = fullArr[i];
 			result += tmpVal;
 		}
 		return result;
