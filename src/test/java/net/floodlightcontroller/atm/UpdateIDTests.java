d4a6059261517558b31e2f1ea55a7b04ee942d70
hanszimmer <michael.weitnau@live.de>
2020-05-09 18:13:51 +0200

    add UpdateID tests

--- src/test/java/net/floodlightcontroller/atm/UpdateIDTests.java
+++ src/test/java/net/floodlightcontroller/atm/UpdateIDTests.java
@@ -1,17 +1,43 @@
 package net.floodlightcontroller.atm;
 
+import net.floodlightcontroller.test.FloodlightTestCase;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-public class UpdateIDTests {
+public class UpdateIDTests extends FloodlightTestCase {
+
+	long testAtmID;
+	UpdateID testUpdateID;
+	byte[] testRestBytes;
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		
+		this.testAtmID = 3;
+		this.testRestBytes = new byte[] { 0, 1, 2 };
+		this.testUpdateID = new UpdateID(this.testAtmID);
+		this.testUpdateID.restBytes = this.testRestBytes;
+	}
+
+	@Test
+	public void whenOfValue_thenCorrect() {
+		long testRaw = this.testUpdateID.toLong();
+		UpdateID test2 = UpdateID.ofValue(testRaw);
+		Assert.assertEquals(this.testUpdateID.toString(), test2.toString());
+	}
 
 	@Test
 	public void whenToLong_thenCorrect() {
-		UpdateID test = new UpdateID(3);
-
-		long result = test.toLong();
+		long result = this.testUpdateID.toLong();
 		System.out.println(result);
-		
 		Assert.assertTrue(result > 3);
+	}
+
+	@Test
+	public void whenToString_thenCorrect() {
+		String output =  this.testUpdateID.toString();
+		Assert.assertEquals("UpdateID: atmID(03) restBytes(00 01 02 ))", output);
 	}
 }
