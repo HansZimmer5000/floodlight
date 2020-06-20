e732ea5b85f4153d5fb24046443d172f9ad1c959
HansZimmer <michael.weitnau@live.de>
2020-06-20 14:22:23 +0200

    remove unused imports

--- src/main/java/net/floodlightcontroller/atm/ATMRoutable.java
+++ src/main/java/net/floodlightcontroller/atm/ATMRoutable.java
@@ -2,14 +2,10 @@ package net.floodlightcontroller.atm;
 
 import org.restlet.Context;
 import org.restlet.Restlet;
-import org.restlet.data.Status;
-import org.restlet.resource.Put;
-import org.restlet.resource.ServerResource;
 import org.restlet.routing.Router;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.floodlightcontroller.core.internal.IOFSwitchService;
 import net.floodlightcontroller.restserver.RestletRoutable;
 
 public class ATMRoutable implements RestletRoutable {
